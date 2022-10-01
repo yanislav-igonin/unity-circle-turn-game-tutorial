54507879ba7b99a1ee3a75d22dc811bfdf8c6e4a
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-01 11:37:05 +0300

    add total money

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public int money;
+    public int earnedMoney;
+    public TextMeshProUGUI moneyText;
+
+    public void Start() {
+        money = PlayerPrefs.GetInt("Money");
+        earnedMoney = PlayerPrefs.GetInt("Score");
+        money += earnedMoney;
+        PlayerPrefs.SetInt("Money", money);
+        moneyText.text = "Money: " + money.ToString();
+        earnedMoney = 0;
+        PlayerPrefs.SetInt("Score", earnedMoney);
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene(((byte)Scenes.ScenesEnum.Game));
